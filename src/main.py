f68d23f569b73aeb1909cb4202abf7dc581804f7
ShashiKant <shashikant@paralleldots.com>
2023-03-01 13:27:40 +0530

    Static File Serving

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from tortoise import generate_config
 from tortoise.contrib.fastapi import register_tortoise
+from fastapi.staticfiles import StaticFiles
+
 
 from .config import DBURL
 from .controller import router
@@ -10,6 +12,8 @@ app = FastAPI(title="Restaurant-Time-Management")
 db_config = generate_config(db_url=DBURL, app_modules={"models": ["src.models", "aerich.models"]})
 
 register_tortoise(app=app, config=db_config,generate_schemas=True, add_exception_handlers=True)
+
+app.mount("/reports", StaticFiles(directory="./reports"), name="reports")
 
 app.include_router(
     router=router,
